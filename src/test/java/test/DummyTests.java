1c185833301fe47accc7c73a27d94d395a04409f
Murali Dhandapani <muralindia@gmail.com>
2018-08-07 10:04:36 +0000

    develop update

--- src/test/java/test/DummyTests.java
+++ src/test/java/test/DummyTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class DummyTests {
   protected void run() {
-    System.out.print("Hello 8asd88 Hello");
+    System.out.print("Hello 8asdasasd88 Hello");
   }
 
   @Test public void test1() {
